0c79197a581a472581fa71051c9b26c6588567d8
Mu5tank05 <nathanblaney98@gmail.com>
2018-05-29 12:32:21 +1000

    llol

---
+++ main.js
@@ -0,0 +1,20 @@
+var i = 0;
+var txt = 'We also think we can program.';
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("typewriter").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function responsiveSize() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
